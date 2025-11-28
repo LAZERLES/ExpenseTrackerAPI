108ca65ff75317020357503efea10c4a09a95502
LAZERLES <poompepee@hotmail.com>
2025-11-28 15:43:25 +0700

    fix: update SameSite attribute for JWT cookie to 'none'

--- Controllers/User.Controller.js
+++ Controllers/User.Controller.js
@@ -75,7 +75,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 3600000,
     });
 
